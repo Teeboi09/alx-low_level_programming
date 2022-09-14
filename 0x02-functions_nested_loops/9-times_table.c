0a18122ce92fcc0a074f50360b8c293fe2d9945f
Teeboi09 <emmaijirotimi@gmail.com>
2022-09-13 22:12:15 -0700

    Next commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,14 +17,11 @@ void times_table(void)
 	
 			prod = num * mult;
 
-			if (prod <= 9)
-	
+			if (prod <= 9)	
 				_putchar(' ');
-    			else
-  																										_putchar((prod / 10) + '0');
+    			else   																						        	_putchar((prod / 10) + '0');
 			
-				_putchar((prod % 10) + '0');
-																									}
-		_putchar('\n');
-}
+				_putchar((prod % 10) + '0');																			}
+			_putchar('\n');
+	}
 }
