93839398d0eb9557578887982c934056aba5e4f5
Teeboi09 <emmaijirotimi@gmail.com>
2022-09-15 22:18:42 -0700

    Next commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,31 +11,30 @@ int main(void)
 
 	while (num++ < 100)
 	{
-	if ((num % 3 == 0) && (num % 5 == 0))
-	{
-		printf("FizzBuzz ");
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+			{
+				if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}									}
+		else
+		{
+			printf("%d ", num);
+		}
 	}
-	else if ((num % 3) == 0)
-	{
-		printf("Fizz ");
-	}
-	else if ((num % 5) == 0)
-	{
-	if (num != 100)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-		printf("Buzz");
-	}
-										}
-else
-{
-	printf("%d ", num);
-}
-}
-printf("\n");
+	printf("\n");
 
-return (0);
+	return (0);
 }
