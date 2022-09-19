ff9b2dab58b79678c248cf847a76fdc75d486de9
Teeboi09 <111147825+Teeboi09@users.noreply.github.com>
2022-09-19 16:37:13 -0700

    Update 5-rev_string.c
    My next commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,51 @@
 #include "main.h"
-#include <stdio.h>
+
+int get_length(char *);
 
 /**
- * main - check the code
+ * rev_string - reverses a string
+ * @s: String to be reversed
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
+void rev_string(char *s)
 {
-	char s[10] = "Holberton";
+	int length;
+	int i;
+	int j;
 
-	printf("%s\n", s);
-        rev_string(s);
-        printf("%s\n", s);
-	return (0);
+	length = get_length(s);
+
+	for (i = 0, j = length - 1; !(i >= j); i++, j--)
+	{
+		char temp;
+
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+
 }
+
+/**
+ * get_length - returns length of String
+ * @s: String length we want to determine
+ *
+ * Return: Length of String
+ */
+int get_length(char *s)
+{
+	int length;
+	int i;
+
+	length = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++length;
+		i++;
+	}
+
+	return (length);
+}
+
