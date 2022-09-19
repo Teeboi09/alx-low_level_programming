ef8d58bbabcc942e6192e38364f7bc272451c1e6
Teeboi09 <emmaijirotimi@gmail.com>
2022-09-19 12:33:40 -0700

    My next commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,15 +3,15 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char s[10] = "Holberton";
 
 	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+        rev_string(s);
+        printf("%s\n", s);
 	return (0);
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+int get_length(char *);
+
+/**
+ * puts_half - print the second half of a string
+ * @str: String to be printed out
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+		int length;
+
+		int n;
+		int start;
+		length = get_length(str);
+		if (length % 2 != 0)
+	{
+	n = (length - 1) / 2;
+											start = length - n;
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	}
+	else
+	{
+		n = length / 2;
+												start = length - n;
+		while (str[start] != '\0')
+	{
+											_putchar(str[start]);
+											start++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * get_length - determines the length of a string
+ * @str: String whose length we want
+ *
+ * Return: length
+ */
+int get_length(char *str)
+{
+		int length;
+
+		int i;
+		i = 0;
+		length = 0;
+		while (str[i] != '\0')
+	{
+		++length;
+		i++;
+	}
+	return (length);
+}
