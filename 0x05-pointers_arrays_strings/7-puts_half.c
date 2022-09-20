7c32e711fb4832c7d6104e84b018caeab7ed36b4
Teeboi09 <111147825+Teeboi09@users.noreply.github.com>
2022-09-20 03:27:18 -0700

    Update 7-puts_half.c
    My first commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,61 +1,34 @@
+#include <stdio.h>
 #include "main.h"
-
-int (char *);
-
 /**
- * puts_half - print the second half of a string
- * @str: String to be printed out
- *
- * Return: void
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
+
 void puts_half(char *str)
 {
-	int length;
-	int n;
-	int start;
+	int len, n, i;
 
-	length = get_length(str);
-	if (length % 2 != 0)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		n = (length - 1) / 2;
-		start = length - n;
-			while (str[start] != '\0')
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			putchar(str[start]);
-			start++;
+			printf("%c", str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			printf("%c", str[n + 1]);
 		}
 	}
-	else
-	{
-		n = length / 2;
-		start = length - n;
-		while (str[start] != '\0')
-		{
-			_putchar(str[start]);
-			start++;
-		}
-	}
-	_putchar('\n');
-}
-
-/**
- * get_length - determines the length of a string
- * @str: String whose length we want
- *
- * Return: length
- */
-int get_length(char *str)
-{
-	int length;
-	int i;
-
-	i = 0;
-	length = 0;
-	while (str[i] != '\0')
-	{
-		++length;
-		i++;
-	}
-
-	return (length);
+	printf("\n");
 }
