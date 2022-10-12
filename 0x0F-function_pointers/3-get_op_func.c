12832a20418ef0883d48b2b11f4f24155d656f4a
Teeboi09 <111147825+Teeboi09@users.noreply.github.com>
2022-10-12 13:57:31 -0700

    Update 3-get_op_func.c
    Commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,90 +1,28 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
 
 /**
-* op_add - Adds two numbers @a and @b
-*
-* @a: A number
-*
-* @b: A number
-*
-* Return: Sum of @a and @b
-*/
-
-int op_add(int a, int b)
+ * get_op_func -  code
+ * @s: code
+ * Return: code
+ */
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-/**
-* op_sub - Calculates the difference of @c and @b
-*
-* @a: A number
-*
-* @b: A number
-*
-* Return: Difference of @a and @b
-*/
-
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
-* op_mul - Multiplies two numbers
-*
-* @a: A number
-*
-* @b: A number
-*
-* Return: Product of @a and @b
-*/
-
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
-* op_div - Divides @a by @b
-*
-* @a: A number
-*
-* @b: A number
-*
-* Return: Result of the division of @a by @b without remainder
-*/
-
-int op_div(int a, int b)
-{
-	if (b == 0)
+	while (i < 5)
 	{
-		printf("Error\n");
-		exit(100);
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
 	}
-
-	return (a / b);
-}
-
-/**
-* op_mod - Calculates the remainder of @a divided by @b
-*
-* @a: A number
-*
-* @b: A number
-*
-* Return: Remainder of @a divider by @b
-*/
-
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
+	return (NULL);
 }
