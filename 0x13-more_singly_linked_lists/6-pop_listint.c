168eb93c922b197dbc77e1a0d661bb7783b3a8ee
Teeboi09 <111147825+Teeboi09@users.noreply.github.com>
2022-10-25 12:13:34 -0700

    Create 6-pop_listint.c
    my commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - code
+ * @head: code
+ * Return: code
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *new_node;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	new_node = (*head)->next;
+	free(*head);
+	*head = new_node;
+	return (n);
+}
